94cf6e0d8a588371eb2f8f67bfba1e6126533ba3
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-10-26 21:10:14 -0300

    add app.js to render component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Calculator from "./Components/Calculator";
+import React from "react";
+import './app.css';
+
+function App() {
+  return (
+    <div className="application">
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Calculator from './Components/Calculator.js';
+import App from './App.js';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render (
   <React.StrictMode>
-    <Calculator/>
+    <App />
   </React.StrictMode>
 );
