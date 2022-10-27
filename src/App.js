c535392f75a5515557d631a36ee46c894846af91
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-10-27 11:15:48 -0300

    Add license file and class component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Calculator from './Components/Calculator';
 import './app.css';
 
-function App() {
-  return (
-    <div className="application">
-      <Calculator />
-    </div>
-  );
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="application">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
