3975748ed1ff317222d8992c5b786edec011da83
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-11-02 13:04:14 -0300

    Add and import dependencies React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './Components/Calculator';
 import './app.css';
 
 function App() {
   return (
     <div className="application">
-      <Calculator />
+      <Routes>
+        <Route path="/" element={<Calculator />} />
+      </Routes>
     </div>
   );
 }
