f6057558799f858dbf0a49067d16b5226d0d8e91
ndorrh <ndorrh@gmail.com>
2022-11-03 03:31:21 +0100

    Fix linter error

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
-import { screen } from '@testing-library/dom';
-import userEvent from '@testing-library/user-event';
 import {
-  Home, About, Quotes, NavBar,
+  Home, About, Quotes,
 } from './App';
 
 it('renders correctly', () => {
