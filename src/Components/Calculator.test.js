fa885adbe0fbbaad7d69fd98752ffac88da1f99a
Solly <solobarine@gmail.com>
2022-11-03 14:07:47 +0100

    Add tests

--- src/Components/Calculator.test.js
+++ src/Components/Calculator.test.js
@@ -45,4 +45,10 @@ describe('Test If a user click a button on the Calculator  the display should Up
     userEvent.click(screen.getByRole('button', { name: /=/i }));
     expect(screen.getByText(/48/i)).toBeInTheDocument();
   });
+  it('If a user click 6, +, 8 and then = button the Calculator Screen displays 14', () => {
+    render(<Calculator />);
+    userEvent.click(screen.getByRole('button', { name: /6/i }));
+    userEvent.click(screen.getByRole('button', { name: /8/i }));
+    expect(screen.getByText(/68/i)).toBeInTheDocument();
+  });
 });
